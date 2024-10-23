f01463657e8ce389a583f5fc691a0f73062d1b35
Peiman Abdi <48262789+pabdi@users.noreply.github.com>
2024-10-23 11:12:11 -0700

    Update result.sql

--- result.sql
+++ result.sql
@@ -1,8 +1,1 @@
-{
-  "Keycloak": {
-    "BaseUrl": "https://your-keycloak-server/auth",
-    "Realm": "your-realm",
-    "ClientId": "your-client-id",
-    "ClientSecret": "your-client-secret"
-  }
-}
+
